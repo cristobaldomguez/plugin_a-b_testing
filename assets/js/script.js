574088469c5fc86c4c4b4a5fb29cba101d14adf8
Cristobal Dominguez <cristobald@me.com>
2016-05-19 01:01:35 -0400

    Major changes, focused on fix duplicated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,77 @@
 $('*', document.body).mouseup(function(event) {
 	event.stopPropagation();
 	var userSelection = window.getSelection(); 					// Variable que almacena la selección hecha por la persona
-	var is_click = userSelection.isCollapsed;  					// Para saber si se realizó un click o una selección (Boolean)
+	var hasBeenClicked = userSelection.isCollapsed;  			// Para saber si se realizó un click o una selección (Boolean)
 	var is_contentEditable = $(this).attr('contenteditable');	// Revisa el parent para saber si el texto ya es editable
 	var rangeObject = userSelection.getRangeAt(0); 				// Retorna el string seleccionado
-	var hasBeenEdited = rangeObject.startContainer.parentNode.getAttribute('class') == 'edited' ? true : false; // Retorna si se la seleccionado un elemento anteriormente editado (Boolean)
+	var hasBeenEdited = rangeObject.startContainer.parentNode.getAttribute('class') == 'edited' ? true : false; // Si se ha seleccionado un elemento ya editado (Boolean)
 
-	if (is_click && !is_contentEditable) { 			// Determinar si se hizo un click en un campo no editable
-		//console.log('Se hizo un click en un campo no editable');
+	if (!window.myVar) {
+		window.myVar = new ABTest($(this));
+		myVar.wrapContent(); 						// Envolver la selección en un span con una clase específica
+	}
 
-		if (!$(this).attr('contenteditable')) {		// Si el contenido no es editable, hacerlo contenteditable y con clase editable
-			$(this).attr({
-				contenteditable: 'true',
-				class: 'editable'
-			});
-		}
+	if (!is_contentEditable) {						// Si el contenido no es editable, hacerlo contenteditable y con clase editable
+		$(this).makeItEditable();
+	}
 
-	} else if (is_click && is_contentEditable) { 	// Determinar si se hizo un click en un campo editable
-		//console.log('Se hizo un click en un campo editable');
+	if (hasBeenClicked) {
 
-		if (hasBeenEdited) { // Se revisa si se hizo la selección en algún elemento anteriormente editado
+		if (hasBeenEdited) {
 			var params = rangeObject.startContainer.parentNode.getAttribute('style'); // Almacena los estilos de css que fueron agregados a la selección
-			console.log('Style: ' + params);
+			myVar.getCSS(params);
 
-			$('.abt-tb').remove();		 									// Si anteriormente se había abierto un wrapper, se cierra antes de crear el siguiente
-			$(myTest.selectedText).append(myTest.get_toolbox('extended'));	// Crear el toolbox correspondiente
-		}
-
-	} else if (!is_click && is_contentEditable) { 	// Si se hizo una selección en un campo de texto editable
-		//console.log('Se hizo una selección en un campo editable');
-		
-		$('.abt-tb').remove(); // Si anteriormente se había abierto un wrapper, se cierra antes de crear el siguiente
-
-		if (hasBeenEdited) { // Se revisa si se hizo la selección en algún elemento anteriormente editado
-			var params = rangeObject.startContainer.parentNode.getAttribute('style'); // Almacena los estilos de css que fueron agregados a la selección
+			$('.abt-tb').remove();
+			$('.edited').addClass('bg');
+			$(myVar.selectedText).append(myVar.get_toolbox('extended'));	// Crear el toolbox correspondiente
 			console.log(params);
+		}
+
+	} else if (!hasBeenClicked) {
+
+		if (hasBeenEdited) {
+			$('.abt-tb').remove();
+			var params = rangeObject.startContainer.parentNode.getAttribute('style'); // Almacena los estilos de css que fueron agregados a la selección
+			$(myVar.selectedText).append(myVar.get_toolbox('extended'));	// Crear el toolbox correspondiente
+
 		} else {
-			var myTest = new ABTest($(this)); // Instanciar nuevo objeto con el contenido seleccionado
-			myTest.selectHTML(); // Envolver la selección en un span con una clase específica
+			$('.abt-tb').remove();
+			$('.edited').addClass('bg');
+			$(myVar.selectedText).append(myVar.get_toolbox('extended'));	// Crear el toolbox correspondiente
 		}
-
-	} else if (!is_click && !is_contentEditable) { 	// Si se hizo una selección en un campo de texto no editable
-		//console.log('Se hizo una selección en un campo no editable');
-
-		
-		if (!$(this).attr('contenteditable')) { // Si el contenido no es editable, hacerlo contenteditable y con clase editable
-			$(this).attr({
-				contenteditable: 'true',
-				class: 'editable'
-			});
-		}
-
-		var myTest = new ABTest($(this)); 		// Instanciar nuevo objeto con el contenido seleccionado
-		myTest.selectHTML(); 					// Envolver la selección en un span con una clase específica
-
-		$('.abt-tb').remove();		 									// Si anteriormente se había abierto un wrapper, se cierra antes de crear el siguiente
-		$(myTest.selectedText).append(myTest.get_toolbox('extended'));	// Crear el toolbox correspondiente
 	}
 });
 
+$('body').on('click', '.abt-b', function(event) {
+	myVar.changeStyle('b');
+});
+
+$('body').on('click', '.abt-u', function(event) {
+	myVar.changeStyle('u');
+});
+
+$('body').on('click', '.abt-i', function(event) {
+	myVar.changeStyle('i');
+});
+
+
+
+$('body').on('click', '.abt-del', function(event) {
+	$('.abt-tb').remove();
+	$('.edited').contents().unwrap();					// Eliminar el span que envuelve el texto
+	myVar = null;
+	delete myVar;
+});
 
 $('body').on('click', '.abt-cl', function(event) {
-	$('.abt-tb').remove(); 
+	$('.abt-tb').remove();
+	$('.edited.bg').removeClass('bg');
 });
 
-$('body').on('click', 'abt-b', function(event) {
-	
-});
+
+
+
+/*var data = "testVariable";
+var i = "2";
+eval("var temp_" + data + i + "=234;");
+console.log(temp_testVariable2);*/
