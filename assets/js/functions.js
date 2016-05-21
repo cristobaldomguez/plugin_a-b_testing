12af34d74257569acbe7bfe0ea5ad080a7603668
Cristobal Dominguez <cristobald@me.com>
2016-05-21 14:14:15 -0400

    Removed hasId function

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -14,3 +14,15 @@ $.fn.removeStyle = function(style) {
 		});
 	});
 };
+
+$.fn.hasId = function(style) {
+    var flag = 0;
+    sel = this;
+    for (var i = 0; i < sel.rangeCount; i++) {
+        var $sNode = $(sel.getRangeAt(i).startContainer.parentNode);
+        var $eNode = $(sel.getRangeAt(i).endContainer.parentNode);
+        if ($sNode.prop("class") === "edited" && $eNode.prop("class") === "edited") {
+            console.log('Ya fue editado');
+        }
+    }
+}
